7ff2ce3fde7eb722a7bfeba3608f10b9affe490b
R2B Apps <r2b.apps@gmail.com>
2020-12-08 19:35:37 +0100

    download and move files to scripts path

--- portable_folder_structure/backup/scripts/updater.py
+++ portable_folder_structure/backup/scripts/updater.py
@@ -1,12 +1,17 @@
-import urllib, sys, datetime
+import urllib, sys, datetime, shutil
 
 print "Python version", sys.version
 print "Date", datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
 
+scripts_path = "/media/portable/backup/scripts"
 base_url = "https://raw.githubusercontent.com/r2bapps/rpi-kodi/main/portable_folder_structure/backup/scripts/"
 files = ["updater.py", "unrar.py", "cleaner.py", "unzip.py"]
 
 downloaded_files = []
+
+# get scripts_path from command
+if len(sys.argv) >= 2:
+    scripts_path = sys.argv[1]
 
 for filename in files:
     url = base_url + filename
@@ -16,6 +21,10 @@ for filename in files:
     except:
         # do nothing
         print "Failed file", filename
+
+# moves downloaded files to scripts folders
+for file in files:
+    shutil.move(file, scripts_path)
 
 print "Downloading", base_url
 print "Files", files
