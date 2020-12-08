92a50d37d70d19e12df2cdb5f7c08f7ef1687449
R2B Apps <r2b.apps@gmail.com>
2020-12-08 13:47:26 +0100

    unrar different parts

--- portable_folder_structure/backup/scripts/unrar.py
+++ portable_folder_structure/backup/scripts/unrar.py
@@ -4,7 +4,8 @@ print "Python version", sys.version
 print "Date", datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
 
 search_path = "/media/portable/torrent/descargas"
-search_extension = "part01.rar"
+search_extensions = [".rar"]
+parts = [".part01", ".part1"]
 
 compressed_files = []
 to_delete_folders = []
@@ -17,18 +18,20 @@ if len(sys.argv) >= 2:
 for currentpath, folders, files in os.walk(search_path):
     for file in files:
         filename, file_extension = os.path.splitext(file)
-        if search_extension in file.lower():
-            found = os.path.join(currentpath, file)
-            compressed_files.append(filename)
-            try:
-                command = "unrar e '" + found + "' " + search_path
-                result = os.system(command)
-                if result == 0:
-                    folder = os.path.dirname(found)
-                    to_delete_folders.append(folder)
-            except:
-                # do nothing
-                print "Failed file", found
+        if file_extension.lower() in search_extensions:
+            for part in parts:
+                if part in filename.lower():
+                    found = os.path.join(currentpath, file)
+                    compressed_files.append(filename)
+                    try:
+                        command = "unrar e '" + found + "' " + search_path
+                        result = os.system(command)
+                        if result == 0:
+                            folder = os.path.dirname(found)
+                            to_delete_folders.append(folder)
+                    except:
+                        # do nothing
+                        print "Failed file", found
 
 # removes to delete folders
 for folder in to_delete_folders:
